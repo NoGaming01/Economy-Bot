1cd573a30f07dac07c2f77036ec4b69739fa2453
Skele <142816007+NoGaming01@users.noreply.github.com>
2024-02-17 14:08:53 +0530

    Added few more commands

--- Cogs/Utility.py
+++ Cogs/Utility.py
@@ -21,5 +21,49 @@ class Utility(commands.Cog):
 
         await ctx.send(embed=embed)
 
+    @commands.command(name="info", description="Gives information about the bot.")
+    async def info(self, ctx):
+        embed = discord.Embed(
+            title="Bot Information!",
+            description="I am a bot made by no_gaming_01.",
+            color=0x00e28d,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_author(name=self.bot.user, icon_url=self.bot.user.avatar)
+        embed.set_thumbnail(url=self.bot.user.avatar)
+        embed.set_footer(text="Made with ❤️ by no_gaming_01.\n© Binary Bit Lab with Project Light")
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="support", description="Gives the support server of the bot.")
+    async def support(self, ctx):
+        embed = discord.Embed(
+            title="Support Server!",
+            description="Join the support server for the bot.",
+            color=0x00e28d,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_author(name=self.bot.user, icon_url=self.bot.user.avatar)
+        embed.set_thumbnail(url=self.bot.user.avatar)
+        embed.set_footer(text="© Binary Bit Lab with Project Light")
+        embed.add_field(name="Link:", value="https://dsc.gg/binaybitlab", inline=False)
+
+        await ctx.send(embed=embed)
+
+    @commands.command(name="github", description="Gives the github repository of the bot.")
+    async def github(self, ctx):
+        embed = discord.Embed(
+            title="Github Repository!",
+            description="Check out out GitHub Organization.",
+            color=0x00e28d,
+            timestamp=datetime.utcnow()
+        )
+        embed.set_author(name=self.bot.user, icon_url=self.bot.user.avatar)
+        embed.set_thumbnail(url=self.bot.user.avatar)
+        embed.set_footer(text="© Binary Bit Lab with Project Light")
+        embed.add_field(name="Link:", value="https://github.com/binary-bit-lab", inline=False)
+
+        await ctx.send(embed=embed)
+
 def setup(bot):
     bot.add_cog(Utility(bot))
